42673a391f3119304a4b577788dce8fcc1e97ff4
suraj <surajdey8603@gmail.com>
2021-06-26 21:13:03 +0530

    Design Pattern

---
+++ src/com/skd/system/design/SingletonDesignPattern.java
@@ -0,0 +1,55 @@
+package com.skd.system.design;
+
+public class SingletonDesignPattern {
+
+    private static SingletonDesignPattern singletonDesign;
+    private SingletonDesignPattern()
+    {
+
+    }
+    //LAZY INITIALIZATION
+    public static SingletonDesignPattern getInstance()
+    {
+        if(singletonDesign==null)
+        {
+            singletonDesign= new SingletonDesignPattern();
+        }
+        return singletonDesign;
+    }
+
+    //Thread Safe
+    public static synchronized SingletonDesignPattern getInstanceThreadSafe()
+    {
+        if(singletonDesign==null)
+        {
+            singletonDesign= new SingletonDesignPattern();
+        }
+        return singletonDesign;
+    }
+
+    //Thread Safe and Double lock
+    public static  SingletonDesignPattern getInstanceThreadSafeDoubleLock()
+    {
+
+            if (singletonDesign == null) {
+                synchronized (SingletonDesignPattern.class) {
+                    if(singletonDesign == null) {
+                        singletonDesign= new SingletonDesignPattern();
+                    }
+            }
+        }
+        return singletonDesign;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(SingletonDesignPattern.getInstance());//SingletonDesignPattern@6acbcfc0
+        System.out.println(SingletonDesignPattern.getInstance());//SingletonDesignPattern@6acbcfc0
+
+        System.out.println(SingletonDesignPattern.getInstanceThreadSafe());//SingletonDesignPattern@6acbcfc0
+        System.out.println(SingletonDesignPattern.getInstanceThreadSafe());//SingletonDesignPattern@6acbcfc0
+
+        System.out.println(SingletonDesignPattern.getInstanceThreadSafeDoubleLock());//SingletonDesignPattern@6acbcfc0
+        System.out.println(SingletonDesignPattern.getInstanceThreadSafeDoubleLock());//SingletonDesignPattern@6acbcfc0
+    }
+}
